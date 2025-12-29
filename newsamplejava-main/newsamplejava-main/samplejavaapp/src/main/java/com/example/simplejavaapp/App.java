afa2bdc27537fd8044fa2a11a51a7007781d6b49
alhaqwalaki786 <alhaqwalaki786@gmail.com>
2025-12-29 12:34:29 +0530

    Add files via upload

---
+++ newsamplejava-main/newsamplejava-main/samplejavaapp/src/main/java/com/example/simplejavaapp/App.java
@@ -0,0 +1,11 @@
+package com.example.simplejavaapp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven! This is a simple Java app.");
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ newsamplejava-main/newsamplejava-main/samplejavaapp/src/test/java/com/example/simplejavaapp/AppTest.java
@@ -0,0 +1,12 @@
+package com.example.simplejavaapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testGreet() {
+        assertEquals("Hello, Sajida!", App.greet("Sajida"));
+    }
+}
